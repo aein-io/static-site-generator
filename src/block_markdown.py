a7e0dafdd383b102481df68016055ebb52e36910
aein <aaeinnor11@gmail.com>
2025-01-28 13:03:34 +0800

    Implemented mardown_to_html_node
    Created functions:
    - mardown_to_html_node
    - block_to_html_node
    - text_to_children
    - heading_to_html
    - code_to_html
    - quote_to_html
    - unordered_list_to_html
    - ordered_list_to_html
    -paragraph_to_html
    
    Created unit tests for functions

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,8 @@
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+
+
 def markdown_to_blocks(markdown):
     blocks = list(
         filter(None, map(lambda x: x.strip(), markdown.split("\n\n"))))
@@ -38,3 +43,105 @@ def block_to_block_type(block):
         return "ordered_list"
 
     return "paragraph"
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+
+    for block in blocks:
+        children.append(block_to_html_node(block))
+
+    return ParentNode("div", children)
+
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+
+    match block_type:
+        case "heading":
+            return heading_to_html(block)
+        case "code":
+            return code_to_html(block)
+        case "quote":
+            return quote_to_html(block)
+        case "unordered_list":
+            return unordered_list_to_html(block)
+        case "ordered_list":
+            return ordered_list_to_html(block)
+        case "paragraph":
+            return paragraph_to_html(block)
+        case _:
+            raise ValueError("Invalid block type")
+
+
+def text_to_children(text):
+    children = []
+    text_nodes = text_to_textnodes(text)
+
+    for text_node in text_nodes:
+        children.append(text_node_to_html_node(text_node))
+
+    return children
+
+
+def heading_to_html(block):
+    hashes = block.count("#")
+
+    if hashes >= len(block) or hashes > 6:
+        raise ValueError(f"Invalid heading: heading {hashes}")
+
+    tag = f"h{hashes}"
+    text = block[hashes + 1:]
+    children = text_to_children(text)
+    return ParentNode(tag, children)
+
+
+def code_to_html(block):
+    text = block[4:-3]
+    children = text_to_children(text)
+    code_node = ParentNode("code", children)
+    return ParentNode("pre", [code_node])
+
+
+def quote_to_html(block):
+    lines = block.split("\n")
+    quote_lines = []
+
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        quote_lines.append(line.lstrip(">").strip())
+
+    quote = " ".join(quote_lines)
+    children = text_to_children(quote)
+    return ParentNode("blockquote", children)
+
+
+def unordered_list_to_html(block):
+    lines = block.split("\n")
+    list_items = []
+
+    for line in lines:
+        children = text_to_children(line[2:])
+        list_items.append(ParentNode("li", children))
+
+    return ParentNode("ul", list_items)
+
+
+def ordered_list_to_html(block):
+    lines = block.split("\n")
+    list_items = []
+
+    for line in lines:
+        children = text_to_children(line[3:])
+        list_items.append(ParentNode("li", children))
+
+    return ParentNode("ol", list_items)
+
+
+def paragraph_to_html(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks, block_to_block_type
+from block_markdown import markdown_to_blocks, block_to_block_type, markdown_to_html_node
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -102,3 +102,126 @@ class BlockToBlockType(unittest.TestCase):
         block = "1. First\n2. Second\n2. Third\n4. Fourth"
         block_type = block_to_block_type(block)
         self.assertEqual(block_type, "paragraph")
+
+
+class TestMarkdownToHtmlNode(unittest.TestCase):
+    def test_paragraph(self):
+        markdown = """
+I am a paragraph with some **bold** text
+Here are some more words in the same
+paragraph
+
+"""
+
+        node = markdown_to_html_node(markdown)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>I am a paragraph with some <b>bold</b> text Here are some more words in the same paragraph</p></div>",
+        )
+
+    def test_paragraphs(self):
+        markdown = """
+I am a paragraph with some **bold** text
+Here are some more words in the same
+paragraph
+
+I am a different paragraph with *italic* text
+
+"""
+        node = markdown_to_html_node(markdown)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>I am a paragraph with some <b>bold</b> text Here are some more words in the same paragraph</p><p>I am a different paragraph with <i>italic</i> text</p></div>"
+        )
+
+    def test_quotes(self):
+        markdown = """
+> This is a quote
+> with multiple lines
+"""
+        node = markdown_to_html_node(markdown)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a quote with multiple lines</blockquote></div>"
+        )
+
+    def test_unordered_list(self):
+        markdown = """
+- List one item one
+- List one item two
+- List one item three
+
+* List two item one
+* List two item two
+* List two item three
+"""
+        node = markdown_to_html_node(markdown)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>List one item one</li><li>List one item two</li><li>List one item three</li></ul><ul><li>List two item one</li><li>List two item two</li><li>List two item three</li></ul></div>"
+        )
+
+    def test_ordered_list(self):
+        markdown = """
+1. First
+2. Second
+3. Third
+4. Fourth
+
+1. First
+2. Second
+2. Third
+3. Fourth
+"""
+        node = markdown_to_html_node(markdown)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>First</li><li>Second</li><li>Third</li><li>Fourth</li></ol><p>1. First 2. Second 2. Third 3. Fourth</p></div>"
+        )
+
+    def test_code(self):
+        markdown = """
+```
+I am a code block
+Here is some code:
+code
+```
+"""
+        node = markdown_to_html_node(markdown)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>I am a code block\nHere is some code:\ncode\n</code></pre></div>"
+        )
+
+    def test_headings(self):
+        markdown = """
+# Heading 1
+
+## Heading 2
+
+### Heading 3
+
+#### Heading 4
+
+##### Heading 5
+
+###### Heading 6
+
+####### Heading 7
+"""
+        node = markdown_to_html_node(markdown)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><h4>Heading 4</h4><h5>Heading 5</h5><h6>Heading 6</h6><p>####### Heading 7</p></div>"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
